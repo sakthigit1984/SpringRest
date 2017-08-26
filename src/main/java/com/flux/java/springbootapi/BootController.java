1a76d8f50dd8d65eef4bba34f8e4455cb3f49618
Sakthivel <sakthi191984@gmail.com>
2017-08-26 10:30:55 -0500

    updated commnet

--- src/main/java/com/flux/java/springbootapi/BootController.java
+++ src/main/java/com/flux/java/springbootapi/BootController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BootController {
 
+    // spting boot mapping
     @RequestMapping("/springbootapp")
     public String dataPush()
     {
