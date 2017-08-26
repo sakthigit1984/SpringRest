fc0e8281e2a4b44b5b4be62ab006eaddee8c6bac
Sakthivel <sakthi191984@gmail.com>
2017-08-26 10:25:07 -0500

    logger added

--- src/main/java/com/flux/java/springbootapi/BootController.java
+++ src/main/java/com/flux/java/springbootapi/BootController.java
@@ -1,14 +1,21 @@
 package com.flux.java.springbootapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootController {
 
+    private final Logger logger= LoggerFactory.getLogger(BootController.class);
+
     @RequestMapping("/springbootapp")
     public String dataPush()
     {
-     return  "spring boot app";
+        String methodName="dataPush";
+        logger.info("Method enter {}",methodName);
+
+        return  "spring boot app";
     }
 }
